8e5bb0590486f76816a19240ceda62f98bf79019
theednaffattack <eddienaff@gmail.com>
2017-09-28 11:04:01 -0700

    Add Redux devTools to store enhancer

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,31 +1,40 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import {routerMiddleware} from 'react-router-redux';
-import {browserHistory} from 'react-router';
-import createLogger from 'redux-logger'
+import { createStore, applyMiddleware, compose } from "redux";
+import { routerMiddleware } from "react-router-redux";
+import { browserHistory } from "react-router";
+import createLogger from "redux-logger";
 
-import DevTools from '../containers/DevTools';
-import promiseMiddleware from '../middleware/promiseMiddleware';
-import getReducer from '../reducers'
+import DevTools from "../containers/DevTools";
+import promiseMiddleware from "../middleware/promiseMiddleware";
+import getReducer from "../reducers";
 
-const middleware = routerMiddleware(browserHistory)
+const middleware = routerMiddleware(browserHistory);
 
-const middlewares = process.env.NODE_ENV === 'development' ?
-    [applyMiddleware(promiseMiddleware, middleware, createLogger()), /*DevTools.instrument()*/] :
-    [applyMiddleware(promiseMiddleware, middleware)];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middlewares =
+  process.env.NODE_ENV === "development"
+    ? [
+        applyMiddleware(
+          promiseMiddleware,
+          middleware,
+          createLogger()
+        ) /*DevTools.instrument()*/
+      ]
+    : [applyMiddleware(promiseMiddleware, middleware)];
 
 export default function configureStore(initialState = {}, apolloClient) {
-    const store = createStore(
-        getReducer(apolloClient),
-        initialState,
-        compose(applyMiddleware(apolloClient.middleware()), ...middlewares)
-    );
-    if (module.hot) {
-        // Enable Webpack hot module replacement for reducers
-        module.hot.accept('../reducers', () => {
-          const nextReducer = require('../reducers/index')(apolloClient).default;
-          store.replaceReducer(nextReducer);
-        });
-    }
+  const store = createStore(
+    getReducer(apolloClient),
+    initialState,
+    composeEnhancers(applyMiddleware(apolloClient.middleware()), ...middlewares)
+  );
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept("../reducers", () => {
+      const nextReducer = require("../reducers/index")(apolloClient).default;
+      store.replaceReducer(nextReducer);
+    });
+  }
 
-    return store;
+  return store;
 }
