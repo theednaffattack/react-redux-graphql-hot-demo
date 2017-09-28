951a908e7bcf5d7879c0d39e258598db1f2d4312
theednaffattack <eddienaff@gmail.com>
2017-09-28 11:05:37 -0700

    Prettier changed formatting, no code changes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,46 +1,39 @@
-import React, {Component} from 'react';
-import { Link } from 'react-router';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { Link } from "react-router";
+import { connect } from "react-redux";
 
-@connect(
-    state=>({
-        isAuthenticated: state.auth.authenticated,
-        loading: state.user.loading,
-        currentUser: state.user.currentUser
-    })
-)
+@connect(state => ({
+  isAuthenticated: state.auth.authenticated,
+  loading: state.user.loading,
+  currentUser: state.user.currentUser
+}))
 export default class Profile extends Component {
-
   render() {
     let { loading, currentUser } = this.props;
-      if (loading) {
-          return (
-              <p className="navbar-text navbar-right">
-              Loading...
-              </p>
-      );
-      } else if (currentUser) {
-          return (
-              <span>
-              <p className="navbar-text navbar-right">
-              Logged as {currentUser.username}
-      &nbsp;&nbsp;
-      <Link to="/logout">Log out</Link>
-          </p>
-          </span>
-      );
-      }
+    if (loading) {
+      return <p className="navbar-text navbar-right">Loading...</p>;
+    } else if (currentUser) {
       return (
+        <span>
           <p className="navbar-text navbar-right">
-          <Link to="/login">Log in</Link>
+            Logged as {currentUser.username}
+            &nbsp;&nbsp;
+            <Link to="/logout">Log out</Link>
           </p>
-  );
+        </span>
+      );
+    }
+    return (
+      <p className="navbar-text navbar-right">
+        <Link to="/login">Log in</Link>
+      </p>
+    );
   }
 }
 
 Profile.propTypes = {
   loading: React.PropTypes.bool,
   currentUser: React.PropTypes.shape({
-    username: React.PropTypes.string.isRequired,
-  }),
+    username: React.PropTypes.string.isRequired
+  })
 };
